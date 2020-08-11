f7af5d882e68d560eb939647f8f1398342230272
Nathan-Patnam <nathanpatnam@hotmail.com>
2020-08-10 21:58:22 -0400

    refactor header tests

--- src/__tests__/component/Header.test.js
+++ src/__tests__/component/Header.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from '../../components/Header';
+import { NAVIGATION_HEADERS } from '../../constants/content';
 
 test('Render correct numbers of nav links', () => {
-  render(<Header />);
-  const aboutButton = screen.getByText(/About/i);
-  const experienceButton = screen.getByText(/Experience/i);
-  const serviceButton = screen.getByText(/Service/i);
-  const projectsButton = screen.getByText(/Projects/i);
-  expect(aboutButton).toBeInTheDocument();
-  expect(experienceButton).toBeInTheDocument();
-  expect(serviceButton).toBeInTheDocument();
-  expect(projectsButton).toBeInTheDocument();
+  const { getByText } = render(<Header />);
+  NAVIGATION_HEADERS.map((navigationHeader) => {
+    expect(getByText(navigationHeader.title)).toBeTruthy();
+  });
 });
