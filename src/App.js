2aa8f6d2ee8d1a87098a156a0f9c5886bd0923a8
Nathan-Patnam <nathanpatnam@hotmail.com>
2020-08-12 01:39:32 -0400

    lower case file name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header';
 import Projects from './screens/projects';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
