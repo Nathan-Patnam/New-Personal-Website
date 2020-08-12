a55033d629166948443a0e67990f0fe2236c4925
Nathan-Patnam <nathanpatnam@hotmail.com>
2020-08-12 02:16:16 -0400

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Project = () => {
-  return <div className='page'>Project</div>;
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+const RecipeReviewCard = ({ props }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={<div>wow</div>}
+        action={<div>View</div>}
+        title={props.title}
+        subheader={props.date}
+      />
+      <CardMedia
+        className={classes.media}
+        image='/static/images/cards/paella.jpg'
+        title='Paella dish'
+      />
+      <CardContent>
+        <Typography variant='body2' color='textSecondary' component='p'>
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label='add to favorites'>Favoruite</IconButton>
+        <IconButton aria-label='share'>Share</IconButton>
+      </CardActions>
+    </Card>
+  );
 };
-
-export default Project;
+export default RecipeReviewCard;
