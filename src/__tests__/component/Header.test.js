a97d00e535cb0a4ff5d8eeefbb13c4c954250e38
Nathan-Patnam <nathanpatnam@hotmail.com>
2020-08-12 02:44:06 -0400

    Fix testing setup in header

--- src/__tests__/component/Header.test.js
+++ src/__tests__/component/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from '../../components/Header';
+import { render } from '@testing-library/react';
+import Header from '../../components/header';
 import { NAVIGATION_HEADERS } from '../../constants/content';
 
 test('Render correct numbers of nav links', () => {
